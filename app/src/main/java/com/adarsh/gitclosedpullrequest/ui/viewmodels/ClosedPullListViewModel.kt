217bb3c67f137070d10967a62fa4bd9eeaf153cd
adarshvris <adarshvris@gmail.com>
2021-08-12 16:29:40 +0530

    Added ViewModels

--- app/src/main/java/com/adarsh/gitclosedpullrequest/ui/viewmodels/ClosedPullListViewModel.kt
+++ app/src/main/java/com/adarsh/gitclosedpullrequest/ui/viewmodels/ClosedPullListViewModel.kt
@@ -1,6 +1,11 @@
 package com.adarsh.gitclosedpullrequest.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.adarsh.gitclosedpullrequest.repo.ClosedPullListRepo
 
-class ClosedPullListViewModel() : ViewModel() {
+class ClosedPullListViewModel(private val repo: ClosedPullListRepo) : ViewModel() {
+
+    fun getClosedPullList() = repo.getClosedPullList().cachedIn(viewModelScope)
 }
