adb8404ce17610a0cd7fd2e5a48edd9e0a7f5548
adarshvris <adarshvris@gmail.com>
2021-08-12 16:29:16 +0530

    Adding repository layer

--- app/src/main/java/com/adarsh/gitclosedpullrequest/repo/ClosedPullListRepo.kt
+++ app/src/main/java/com/adarsh/gitclosedpullrequest/repo/ClosedPullListRepo.kt
@@ -1,6 +1,28 @@
 package com.adarsh.gitclosedpullrequest.repo
 
-import com.adarsh.gitclosedpullrequest.base.network.BaseApiSource
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.adarsh.gitclosedpullrequest.api.ClosedPullApi
+import com.adarsh.gitclosedpullrequest.models.ClosedPullData
+import com.adarsh.gitclosedpullrequest.paging.ClosedPullPagingDataSource
+import com.adarsh.gitclosedpullrequest.paging.ClosedPullPagingDataSource.Companion.NETWORK_PAGE_SIZE
+import kotlinx.coroutines.flow.Flow
 
-class ClosedPullListRepo() : BaseApiSource() {
+
+class ClosedPullListRepo(private val closedPullApi: ClosedPullApi) {
+
+    companion object {
+        const val PREFETCH_DISTANCE = 4
+    }
+
+    fun getClosedPullList(): Flow<PagingData<ClosedPullData>> {
+        return Pager(config = PagingConfig(
+                initialLoadSize = 2 * NETWORK_PAGE_SIZE,
+                pageSize = NETWORK_PAGE_SIZE,
+                prefetchDistance = PREFETCH_DISTANCE,
+                enablePlaceholders = true
+        ), pagingSourceFactory = { ClosedPullPagingDataSource(closedPullApi) }).flow
+    }
+
 }
